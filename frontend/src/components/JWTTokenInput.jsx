929de26a261d7fc937cdd68eb1a1e5638a2d99b6
Umar Khursheed <umarkhursheed979@gmail.com>
2025-05-29 15:38:06 +0530

    updated JWT Input UI

--- frontend/src/components/JWTTokenInput.jsx
+++ frontend/src/components/JWTTokenInput.jsx
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import { useJwt } from "../context/JWTContext";
 import { jwtDecode } from "jwt-decode";
-
+import { EyeOff, Eye } from "lucide-react";
 export default function JwtTokenInput() {
   const { token, setToken } = useJwt();
 
@@ -13,19 +14,28 @@ export default function JwtTokenInput() {
       isExpired = true;
     }
   } catch {}
-
+  const [showJwt, setShowJwt] = useState(false);
   return (
     <div className="bg-white w-[40%] shadow p-4 rounded-lg space-y-2 mb-4 border">
       <label className="block text-sm font-medium text-gray-700">
         JWT Auth Token
       </label>
-      <input
-        type="text"
-        className="w-full border rounded p-2 text-black text-sm"
-        placeholder="Paste your JWT here..."
-        value={token}
-        onChange={(e) => setToken(e.target.value)}
-      />
+      <div className="relative">
+        <input
+          type={showJwt ? "text" : "password"}
+          className="w-full border rounded p-2 text-black text-sm pr-10"
+          placeholder="Paste your JWT here..."
+          value={token}
+          onChange={(e) => setToken(e.target.value)}
+        />
+        <button
+          type="button"
+          onClick={() => setShowJwt(!showJwt)}
+          className="absolute z-50 inset-y-0 right-0 flex items-center px-3 text-black cursor-pointer"
+        >
+          {showJwt ? <EyeOff size={20} /> : <Eye size={20} />}
+        </button>
+      </div>
 
       {token && (
         <div className="flex justify-between items-center text-xs text-gray-600">
