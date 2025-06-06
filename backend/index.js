e7d527cf679bcc259dbd92033aacf3734c211a21
Umar Khursheed <umarkhursheed979@gmail.com>
2025-06-07 03:00:36 +0530

    Updated URL

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: `${process.env.FRONTEND_URL}`,
+    origin: `${process.env.FRONTEND_URL}/`,
     credentials: true,
   })
 );
